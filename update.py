a86d22a936eb09ee59f5098b89ee420a4f12cf3b
Patrick Tuffy <tuffyp@tcd.ie>
2018-04-11 15:46:42 +0100

    date and time working

--- update.py
+++ update.py
@@ -1,9 +1,14 @@
 from lib import JCDecaux as jcd
 import time
 
-city = 'Dublin'
+city = "Dublin"
+
+times = time.strftime("%Y%m%d-%H%M%S")
+
+citytimes= city + times
 
 while True:
     dataframe = jcd.information(city)
-    dataframe.to_csv('static/data/{0}.csv'.format(city), index=False)
-    time.sleep(60)
+    dataframe.to_csv('static/data/{0}.csv'.format(citytimes), index=False)
+    time.sleep(300)
+
