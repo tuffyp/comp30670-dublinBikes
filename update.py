4fde32548c936b9e7d823172991d5c0a3c1b443e
Patrick Tuffy <tuffyp@tcd.ie>
2018-04-16 15:29:52 +0100

    filename changed back to Dublin.csv so live info can be taken from CSV file and not via a call to our database (which doesnt exist yet

--- update.py
+++ update.py
@@ -3,12 +3,12 @@ import time
 
 city = "Dublin"
 
-times = time.strftime("%Y%m%d-%H%M%S")
+#times = time.strftime("%Y%m%d-%H%M%S")
 
-citytimes= city + times
+#citytimes= city + times
 
 while True:
     dataframe = jcd.information(city)
-    dataframe.to_csv('static/data/{0}.csv'.format(citytimes), index=False)
+    dataframe.to_csv('static/data/{0}.csv'.format(city), index=False)
     time.sleep(300)
 
